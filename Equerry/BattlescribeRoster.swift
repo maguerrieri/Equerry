26bd42fa9681eba5a8fb1e15aeeade21ea56af56
Mario Guerrieri <mario@guerrieri.codes>
2022-08-06 12:35:59 -0700

    Load profiles

--- Equerry/BattlescribeRoster.swift
+++ Equerry/BattlescribeRoster.swift
@@ -73,17 +73,43 @@ struct BattlescribeRoster: Codable, DynamicNodeCoding, Identifiable {
             let id: String
             let name: String
 
+            let profiles: Profiles?
+
+            struct Profiles: Codable {
+                let profile: [Profile]
+            }
+            struct Profile: Codable, DynamicNodeCoding, Identifiable {
+                let id: String
+                let name: String
+
+                enum CodingKeys: String, CodingKey {
+                    case id
+                    case name
+                }
+
+                fileprivate static func nodeCoding(for key: CodingKey) -> NodeCoding {
+                    switch key {
+                        case CodingKeys.id, CodingKeys.name:
+                            return .attribute
+                        default:
+                            return .element
+                    }
+                }
+            }
+
             enum CodingKeys: String, CodingKey {
                 case id
                 case name
+
+                case profiles
             }
 
             fileprivate static func nodeCoding(for key: CodingKey) -> NodeCoding {
                 switch key {
-                    case CodingKeys.name:
+                    case CodingKeys.id, CodingKeys.name:
                         return .attribute
                     default:
-                        return .attribute
+                        return .element
                 }
             }
 
@@ -107,7 +133,7 @@ struct BattlescribeRoster: Codable, DynamicNodeCoding, Identifiable {
 
         fileprivate static func nodeCoding(for key: CodingKey) -> NodeCoding {
             switch key {
-                case CodingKeys.name, CodingKeys.id:
+                case CodingKeys.id, CodingKeys.name:
                     return .attribute
                 default:
                     return .element
@@ -120,7 +146,23 @@ struct BattlescribeRoster: Codable, DynamicNodeCoding, Identifiable {
 """
 <force id="ccc1-75f4-c775-76eb" name="**Pitched Battle GHB 2022** 1,000" entryId="1411-460f-c135-a667" catalogueId="e23c-3deb-f35c-8104" catalogueRevision="38" catalogueName="Order - Kharadron Overlords">
     <selections>
-        <selection id="5c41-69c8-4bee-2c7e" name="Core Battalion: Battle Regiment" entryId="eb9a-ad10-f507-1b3a::1878-56e2-2862-8da5" number="1" type="upgrade" />
+        <selection id="5c41-69c8-4bee-2c7e" name="Core Battalion: Battle Regiment" entryId="eb9a-ad10-f507-1b3a::1878-56e2-2862-8da5" number="1" type="upgrade">
+            <profiles>
+                <profile id="eb9a-ad10-f507-1b3a::367e-fa52-1a3d-0684" name="Core Battalion: Battle Regiment" hidden="false" typeId="75e0-a332-e4f5-bf36" typeName="Battalion Organisation">
+                    <characteristics>
+                        <characteristic name="Required" typeId="eb5f-e9d2-e457-bff5">1 Commander
+0-2 Sub-commanders
+2-5 Troops
+0-1 Monster or Artillery</characteristic>
+                    </characteristics>
+                </profile>
+                <profile id="eb9a-ad10-f507-1b3a::2dca-6135-9464-0728::9439-c5f6-945d-2d65" name="Unified" hidden="false" typeId="bdc6-78da-3796-60a3" typeName="Battalion Abilities">
+                    <characteristics>
+                        <characteristic name="Battalion Ability Details" typeId="08e0-9ead-1dbe-c801">One-drop Deployment (see 26.2.1).</characteristic>
+                    </characteristics>
+                </profile>
+            </profiles>
+        </selection>
     </selections>
 </force>
 """
@@ -163,6 +205,14 @@ struct BattlescribeRoster: Codable, DynamicNodeCoding, Identifiable {
 
 #if DEBUG
 struct BattlescribeRoster_Previews: PreviewProvider {
+    private struct ProfileTester: View {
+        let profile: BattlescribeRoster.Force.Selection.Profile
+
+        var body: some View {
+            Text(profile.name)
+        }
+    }
+
     private struct SelectionTester: View {
         let selection: Result<BattlescribeRoster.Force.Selection, Error>
 
@@ -171,6 +221,13 @@ struct BattlescribeRoster_Previews: PreviewProvider {
                 case .success(let selection):
                     VStack {
                         Text(selection.name)
+                            .font(.title3)
+
+                        if let profiles = selection.profiles {
+                            ForEach(profiles.profile) { profile in
+                                ProfileTester(profile: profile)
+                            }
+                        }
                     }
                 case .failure(let error):
                     Text("\(String(reflecting: error))")
@@ -186,7 +243,7 @@ struct BattlescribeRoster_Previews: PreviewProvider {
                 case .success(let force):
                     VStack {
                         Text(force.name)
-                            .font(.headline)
+                            .font(.title2)
 
                         ForEach(force.selections.selection) { selection in
                             SelectionTester(selection: .success(selection))
@@ -218,6 +275,7 @@ struct BattlescribeRoster_Previews: PreviewProvider {
 
     static var previews: some View {
         RosterTester()
+            .previewLayout(.sizeThatFits)
 
         ForceTester(force: BattlescribeRoster.Force.test)
 
