179443857df3a9d6bd35870660c9f24f2b65d4dd
Mario Guerrieri <mario@guerrieri.codes>
2023-06-08 16:55:40 -0700

    Switch to SwiftData

--- Equerry/App.swift
+++ Equerry/App.swift
@@ -5,15 +5,15 @@
 //  Created by Mario Guerrieri on 8/3/22.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
 struct App: SwiftUI.App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             RootView()
         }
+        .modelContainer(for: [Item.self])
     }
 }
---
+++ Equerry/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Equerry
+//
+//  Created by Mario Guerrieri on 6/6/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Item {
+    let timestamp: Date
+}
